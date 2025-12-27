b97a1254e914ea4e135063355dec7975f5916108
TwoBitCoder <dave.steenburgh@gmail.com>
2025-12-27 16:46:10 -0500

    Boilerplate with execution timing and .gitignore.

---
+++ base.py
@@ -0,0 +1,19 @@
+import time
+
+def main():
+  """Solution for Advent of Code 2025, XX."""
+  # Start timing once we're in main(). Initialization of variables is included.
+  start_time = time.perf_counter()
+
+  # Show your work here.
+
+  # End timing once we have the result.
+  end_time = time.perf_counter()
+
+  # Output result - do not include printing in time measurement.
+  print(f"Result: {result}")
+  calc_time = end_time - start_time
+  print(f"Calculation time: {calc_time:.4f} seconds")
+
+if __name__ == "__main__":
+  main()
